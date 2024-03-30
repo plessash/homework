669739be62e7df72b02cd9e226c2ce854009ab46
mac <ples.sash@gmail.com>
2024-03-30 15:18:26 +0300

    com1

---
+++ src/dz_5/Ex1_2.java
@@ -0,0 +1,11 @@
+package dz_5;
+
+public class Ex1_2 {
+    public static void main(String[] args) {
+String str = "Вывести в консоль строку посимвольно";
+char[] array = str.toCharArray();
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
---
+++ src/dz_5/Ex1_3.java
@@ -0,0 +1,13 @@
+package dz_5;
+
+public class Ex1_3 {
+    public static void main(String[] args) {
+        String number = "012312";
+        long num = Long.parseLong(number);
+        System.out.println(num);
+
+        Long numberLong = Long.valueOf(number);
+        long num2 = numberLong.longValue();
+        System.out.println(num2);
+    }
+}
