22e589ef8c84cc26586e97397ad9f6feec4ec761
mac <ples.sash@gmail.com>
2024-04-23 00:54:58 +0300

    1 and 2 done!

--- src/dz_7_Exception/ex_2/Method1.java
+++ src/dz_7_Exception/ex_2/Method1.java
@@ -3,7 +3,9 @@ public class Method1 {
     public boolean isEven(int value) throws CustomException, CustomException2 {
         boolean isEven;
         if (isEven = (value % 2) == 0) {
-            return isEven;
+            System.out.println(value + " является четным");
+            return true;
+
         } else {
             throw new CustomException("Число не четное!");
         }
