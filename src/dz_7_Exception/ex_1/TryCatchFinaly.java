70c638cb7afc10e0181a145e5e06f80458b14f49
mac <ples.sash@gmail.com>
2024-05-02 01:31:10 +0300

    3 done

--- src/dz_7_Exception/ex_1/TryCatchFinaly.java
+++ src/dz_7_Exception/ex_1/TryCatchFinaly.java
@@ -2,36 +2,47 @@ package dz_7_Exception.ex_1;
 
 public class TryCatchFinaly {
     public static void main(String[] args) throws ArithmeticException {
-        int num = 0;
-        int num1 = 5;
-        int result;
+        task1();
+       // task2();
+       // task3();
+    }
 
+    private static int task1() {
+        int a = 5;
+        int b = 0;
         try {
-            result = num1 / num;
-            System.out.println(result);
+            int result = a / b;
         } catch (ArithmeticException e) {
-            System.out.println("Ошибка: деление на ноль");
+            System.out.println("Нельзя делить на ноль");
         }
+        return 0;
+    }
 
+    private static int task2() {
+        int a = 5;
+        int b = 0;
         try {
-            result = num1 / num;
-            System.out.println(result);
+            int result = a / b;
         } finally {
-            if (num == 0) {
-                num = 1;}
-            result = num1 / num;
-            System.err.println("Выполняем деление на 1. Результат = " + result);
+            b = 1;
+            int result2 = a / b;
+            System.err.println("Ошибка! Деление на 0 невозможно!");
+            System.out.println("Значение 0 заменено на 1. Результат = " + result2);
         }
+        return 0;
+    }
 
+    private static int task3() {
+        int a = 5;
+        int b = 0;
         try {
-            result = num1 / num;
+            int result = a / b;
             System.out.println(result);
         } catch (ArithmeticException e) {
             System.out.println("Ошибка: деление на ноль");
         } finally {
             System.out.println("Введите число отличное от 0");
         }
+        return 0;
     }
-
 }
-
